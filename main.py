124d5c627c3dad11a4f30c8b817a219e841abb36
E H Murdoch <ehmurdoch47@protonmail.com>
2021-06-25 20:40:19 +0100

    added function to display species data

--- main.py
+++ main.py
@@ -79,7 +79,6 @@ def inputSpeciesData ():
             count = int ( user_input )
 
             current_record = Species ( name, count )
-            
             species_data.append ( current_record )
 
             print ()
@@ -88,5 +87,80 @@ def inputSpeciesData ():
 
     print ()
     print ( "Finished inputting species data." )
+    
+    print ()
 
     return species_data
+
+def getWidthForNameColumn ( species_data ):
+    if len ( species_data ) != 0:
+        name_lengths = [ len ( record.name ) for record in species_data ]
+        max_name_length = max ( name_lengths )
+    else:
+        max_name_length = 0
+
+    # len ( "Name" ) == 4
+    # This ensures that the width for the Name column is not shorter than the
+    # literal string "Name", since we are displaying the header too.
+    width_for_name_column = max ( 4, max_name_length )
+    # this is a constant gap that will be there regardless
+    width_for_name_column += 4
+
+    return width_for_name_column
+
+def getWidthForCountColumn ( species_data ):
+    if len ( species_data ) != 0:
+        count_lengths = [ len ( str ( record.count ) ) for record in species_data ]
+        max_count_length = max ( count_lengths )
+    else:
+        max_count_length = 0
+
+    # len ( "Count" ) == 5
+    # This ensures that the width for the Count column is not shorter than the
+    # literal string "Count", since we are displaying the header too.
+    width_for_count_column = max ( 5, max_count_length )
+    # this is a constant space that will be there regardless
+    width_for_count_column += 1
+
+    return width_for_count_column
+
+def printTableHeader ( width_for_name_column, width_for_count_column ):
+    total_width = width_for_name_column + width_for_count_column
+
+    name_cell = "Name".ljust ( width_for_name_column )
+    count_cell = "Count".ljust ( width_for_count_column )
+
+    row_string = name_cell + count_cell
+
+    print ( row_string )
+
+    row_string = '-' * total_width
+
+    print ( row_string )
+
+def printRecordForTable ( width_for_name_column, width_for_count_column, record ):
+    name_cell = record.name.ljust ( width_for_name_column )
+    count_cell = str ( record.count ).ljust ( width_for_count_column )
+
+    row_string = name_cell + count_cell
+
+    print ( row_string )
+
+def displaySpeciesDataAsTable ( species_data ):
+    width_for_name_column = getWidthForNameColumn ( species_data )
+    width_for_count_column = getWidthForCountColumn ( species_data )
+
+    printTableHeader ( width_for_name_column, width_for_count_column )
+
+    for record in species_data:
+        printRecordForTable ( width_for_name_column, width_for_name_column, record )
+
+    print ()
+
+def main ():
+    species_data = inputSpeciesData ()
+
+    displaySpeciesDataAsTable ( species_data )
+
+if __name__ == '__main__':
+    main ()
