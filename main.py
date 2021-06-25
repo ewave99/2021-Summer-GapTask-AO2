db24b71e0934954674f8fde19778dc40e1529a4b
E H Murdoch <ehmurdoch47@protonmail.com>
2021-06-25 09:45:01 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+"""
+A botanist wants to be able to record the species they find in a meadow.
+
+They use a quadrat to select 5 areas of the meadow at various points of the
+year.
+
+They would like to be able to record the different species they see and how
+many plants of each species are present.
+
+Program requirements:
+    - Allows the data to be collected and displayed in a tabulated format
+    - Allows the user to edit the collected data
+    - Allows the user to save the collected data in a CSV format
+    - Allow data from previous years to be loaded from a CSV file in order to
+      be edited, displayed or resaved
+    - Display the species seen over the year to be displayed in a simple graph
+      to show how many times each species has been found.
+"""
