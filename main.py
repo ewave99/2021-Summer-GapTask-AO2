b50d88f3d38e78c4ee5d18be2d2e58e05028cf55
E H Murdoch <ehmurdoch47@protonmail.com>
2021-06-25 11:11:48 +0100

    Implemented species-inputting mechanism

--- main.py
+++ main.py
@@ -16,3 +16,61 @@ Program requirements:
     - Display the species seen over the year to be displayed in a simple graph
       to show how many times each species has been found.
 """
+
+from collections import namedtuple
+import re
+
+Species = namedtuple ( typename="Species", field_names=[ "name", "count" ] )
+
+species_data = []
+
+def validateSpeciesName ( string_to_test ):
+    regex_to_match = re.compile ( r"^[a-z]+([\s][a-z]+)*$" )
+
+    if regex_to_match.match ( string_to_test.lower () ):
+        return True
+    
+    return False
+
+def validateSpeciesCount ( string_to_test ):
+    regex_to_match = re.compile ( r"^[0-9]+$" )
+
+    if regex_to_match.match ( string_to_test ):
+        return True
+
+    return False
+
+def inputSpeciesData ():
+    print ( "Inputting species data. Leave either field blank to stop." )
+
+    prompt_for_name = "Enter species name: "
+    prompt_for_count = "Enter specimen count: "
+
+    user_input = input ( prompt_for_name )
+
+    while validateSpeciesName ( user_input ) == False and user_input != "":
+        print ( "Invalid name." )
+
+        user_input = input ( prompt_for_name )
+
+    while user_input != "":
+        name = user_input
+
+        user_input = input ( prompt_for_count )
+
+        while validateSpeciesCount ( user_input ) == False and user_input != "":
+            print ( "Invalid count." )
+
+            user_input = input ( prompt_for_count )
+
+        if user_input != "":
+            count = user_input
+
+            print ()
+
+            user_input = input ( prompt_for_name )
+
+            while validateSpeciesName ( user_input ) == False and user_input != "":
+                print ( "Invalid name." )
+
+                user_input = input ( prompt_for_name )
