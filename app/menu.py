556dcc359efe3ed697b75856f43235e643020d8e
E H Murdoch <ehmurdoch47@protonmail.com>
2021-06-28 09:40:22 +0100

    Moved main menu options function into MainMenu class situated in app/menu.py.

--- app/menu.py
+++ app/menu.py
@@ -1,5 +1,12 @@
 from .generic import GenericMethods
 
+from .data_display    import DataDisplayMethods
+from .data_input      import DataInputMethods
+from .record_editing  import RecordEditingMethods
+from .record_deleting import RecordDeletingMethods
+from .sorting         import SortingMethods
+from .csv_file_io     import CSVFileIOMethods
+
 class MainMenuMethods ( GenericMethods ):
     def __init__ ( self ):
         super ( MainMenuMethods, self ).__init__ ()
@@ -32,3 +39,57 @@ class MainMenuMethods ( GenericMethods ):
         print ()
 
         return user_input
+
+class MainMenu (
+        DataDisplayMethods,
+        DataInputMethods,
+        MainMenuMethods,
+        RecordEditingMethods,
+        RecordDeletingMethods,
+        SortingMethods,
+        CSVFileIOMethods
+        ):
+    def __init__ ( self ):
+        super ( MainMenu, self ).__init__ ()
+
+    def main ( self ):
+        while True:
+            self.displayMainMenu ()
+
+            main_menu_choice = int ( self.inputMainMenuChoice () )
+
+            if main_menu_choice == 1:
+                self.displaySpeciesDataAsTable ()
+
+            elif main_menu_choice == 2:
+                self.displaySpeciesDataAsBarChart ()
+
+            elif main_menu_choice == 3:
+                self.inputSpeciesData ()
+
+            elif main_menu_choice == 4:
+                self.pickAndEditRecord ()
+
+            elif main_menu_choice == 5:
+                self.pickAndDeleteRecord ()
+
+            elif main_menu_choice == 6:
+                self.sortRecords ()
+
+            elif main_menu_choice == 7:
+                self.clearCurrentRecords ()
+
+            elif main_menu_choice == 8:
+                self.saveAsCSV ()
+
+            elif main_menu_choice == 9:
+                self.loadDataFromCSV ()
+
+            elif main_menu_choice == 10:
+                confirmation = input ( "Are you sure you want to quit [y/n]: " )
+                
+                if confirmation.lower () == 'y':
+                    print ( "QUITTING." )
+                    break
+                else:
+                    print ()
