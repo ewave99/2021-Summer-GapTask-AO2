c882d66a9f4bb8cf126ea5962ca8a854c9436662
E H Murdoch <ehmurdoch47@protonmail.com>
2021-06-28 10:44:03 +0100

    added comments to data_input.py

--- app/data_input.py
+++ app/data_input.py
@@ -5,8 +5,6 @@ from .generic import Species
 # Methods to do with inputting species data
 class DataInputMethods:
     def inputSpeciesData ( self ):
-        current_record = None
-
         print ( "INPUT SPECIES DATA:" )
 
         print ( "Inputting species data. Leave either field blank to stop." )
@@ -14,15 +12,19 @@ class DataInputMethods:
 
         user_input = self.inputSpeciesName ()
 
+        # input data until any of the input fields == "".
         while user_input != "":
             name = user_input
 
             user_input = self.inputSpeciesCount ()
 
+            # this if statement is needed since we are checking *both* fields
+            # for a blank input.
             if user_input != "":
                 count = int ( user_input )
 
                 current_record = Species ( name, count )
+
                 self.species_data.append ( current_record )
 
                 print ()
@@ -39,8 +41,12 @@ class DataInputMethods:
     def inputSpeciesName ( self ):
         user_input = input ( "Enter species name: " )
 
+        # True or False
         is_valid = self.validateSpeciesName ( user_input )
 
+        # "" will not be considered an invalid name. This is because if the
+        # value is "", then the program needs it to determine whether to
+        # terminate the data-inputting process.
         while is_valid == False and user_input != "":
             print ( "Invalid name." )
 
@@ -48,6 +54,7 @@ class DataInputMethods:
             
             is_valid = self.validateSpeciesName ( user_input )
 
+        # True or False
         name_already_exists = self.checkIfNameAlreadyExists ( user_input )
 
         while name_already_exists and user_input != "":
@@ -77,15 +84,22 @@ class DataInputMethods:
         return False
 
     def checkIfNameAlreadyExists ( self, string_to_test ):
+        # using str.lower () further decreases the likelihood of the user
+        # accidentally inputting duplicate species.
         reference_string = string_to_test.lower ()
 
         name_already_exists = False
 
         i = 0
 
+        # walk through all the records and see if there is a name that matches
         while name_already_exists == False and i < len ( self.species_data ):
             record = self.species_data [ i ]
 
+            # as mentioned above, we compare the lowercase versions of the
+            # test string and the record name. Therefore, say, "Banana" will
+            # be considered to be the same as "banana". So, overall we can say
+            # that the species name is case-insensitive.
             if reference_string == record.name.lower ():
                 name_already_exists = True
 
@@ -106,6 +120,8 @@ class DataInputMethods:
         return user_input
 
     def validateSpeciesCount ( self, string_to_test ):
+        # checks if the input is a valid number. Shorter than writing a
+        # try...except block with an attempt of int ( string_to_test ) .
         regex_to_match = re.compile ( r"^[0-9]+$" )
 
         if regex_to_match.match ( string_to_test ):
